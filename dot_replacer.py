f0dd05d0cd99df81f6cb7c8366b4a4f49f4ef2b4
Herman Havva <108483440+hermanhavva@users.noreply.github.com>
2024-12-17 14:09:19 +0200

    ensure the bounds

--- dot_replacer.py
+++ dot_replacer.py
@@ -5,14 +5,15 @@ while(user_input != "exit"):
     user_input = input("Enter the text:\n")
     result_str = ""
     for index in range(0, len(user_input)):
-        if (index > 0 and index < len(user_input) and user_input[index] == "."):
+        if (index > 0 and index < len(user_input) - 1 and user_input[index] == "."):
             if (user_input[index - 1].isnumeric() and user_input[index + 1].isnumeric()):
                 result_str += ','
             else:
                 result_str += user_input[index]
         else:
             result_str += user_input[index]
-    pyperclip.copy(result_str)
-    print(f"The string was copied to the clipboard")
+    if (user_input != "exit"):
+        pyperclip.copy(result_str)
+        print(f"The string was copied to the clipboard")
 
 
