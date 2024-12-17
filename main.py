488587ee6ad2ea63a31053256bd62089396ba4e0
Herman Havva <108483440+hermanhavva@users.noreply.github.com>
2024-12-17 12:48:28 +0200

    Init commit, add script, add gitignore

---
+++ main.py
@@ -0,0 +1,18 @@
+import pyperclip
+
+user_input = ""
+while(user_input != "exit"):
+    user_input = input("Enter the text:\n")
+    result_str = ""
+    for index in range(0, len(user_input)):
+        if (index > 0 and index < len(user_input) and user_input[index] == "."):
+            if (user_input[index - 1].isnumeric() and user_input[index + 1].isnumeric()):
+                result_str += ','
+            else:
+                result_str += user_input[index]
+        else:
+            result_str += user_input[index]
+    pyperclip.copy(result_str)
+    print(f"The string was copied to the clipboard")
+
+
