d4ac52b2b88694c9a9913e043d72ff199b117539
Herman Havva <108483440+hermanhavva@users.noreply.github.com>
2024-12-17 16:08:36 +0200

    add clearing the screen and try and catch logic

--- dot_replacer.py
+++ dot_replacer.py
@@ -1,20 +1,25 @@
 import pyperclip
+import os
 
 user_input = ""
-while user_input != "exit":
-    user_input = input("Enter the text:\n")
-    result_str = ""
-    for index in range(0, len(user_input)):
-        if index > 0 and index < len(user_input) - 1 and user_input[index] == ".":
-            if user_input[index - 1].isnumeric() and user_input[index + 1].isnumeric():
-                result_str += ','
+try:
+    while user_input != "exit":
+        user_input = input("Enter the text:\n")
+        os.system('cls')
+        result_str = ""
+        for index in range(0, len(user_input)):
+            if index > 0 and index < len(user_input) - 1 and user_input[index] == ".":
+                if user_input[index - 1].isnumeric() and user_input[index + 1].isnumeric():
+                    result_str += ','
+                else:
+                    result_str += user_input[index]
             else:
                 result_str += user_input[index]
-        else:
-            result_str += user_input[index]
 
-    if user_input != "exit":
-        pyperclip.copy(result_str)
-        print(f"The string was copied to the clipboard")
+        if len(user_input.split()) > 0 and user_input.split()[0] != "exit":
+            pyperclip.copy(result_str)
+            print("The string was copied to the clipboard")
+except Exception as e:
+    print(f"An error occurred: {e}")
 
 
