6ccf10e471da099aa52f1779ed9d4324e55378a2
Herman Havva <108483440+hermanhavva@users.noreply.github.com>
2024-12-17 14:13:07 +0200

    Update dot_replacer.py

--- dot_replacer.py
+++ dot_replacer.py
@@ -1,18 +1,19 @@
 import pyperclip
 
 user_input = ""
-while(user_input != "exit"):
+while user_input != "exit":
     user_input = input("Enter the text:\n")
     result_str = ""
     for index in range(0, len(user_input)):
-        if (index > 0 and index < len(user_input) - 1 and user_input[index] == "."):
-            if (user_input[index - 1].isnumeric() and user_input[index + 1].isnumeric()):
+        if index > 0 and index < len(user_input) - 1 and user_input[index] == ".":
+            if user_input[index - 1].isnumeric() and user_input[index + 1].isnumeric():
                 result_str += ','
             else:
                 result_str += user_input[index]
         else:
             result_str += user_input[index]
-    if (user_input != "exit"):
+
+    if user_input != "exit":
         pyperclip.copy(result_str)
         print(f"The string was copied to the clipboard")
 
